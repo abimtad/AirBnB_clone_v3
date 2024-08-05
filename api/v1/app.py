bf0364f6a59723fdde9418cbb671bf2471df3e0b
Abel Tadesse <tadesseabel26@gmail.com>
2024-08-05 23:49:58 +0300

    task_3

---
+++ api/v1/app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+""" A Flask Application """
+from api.v1.views import app_views
+from models import storage
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flasgger import Swagger
+from flask_cors import CORS
+from flasgger.utils import swag_from
+
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close the Storage """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error:
+    ----------------
+    responses:
+      404:
+        description: a resource was not found
+
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
+
+if __name__ == "__main__":
+    """ The main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not port:
+        port = '5000'
+    if not host:
+        host = '0.0.0.0'
+
+    app.run(host=host, port=port, threaded=True)
